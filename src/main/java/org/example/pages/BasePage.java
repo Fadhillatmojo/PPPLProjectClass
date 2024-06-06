7fb9f70abd68ac5d38cee0c0649179b25b31fb2e
User <fadhillatmojo@gmail.com>
2024-06-06 14:09:55 +0700

    feat: google test

--- src/main/java/org/example/pages/BasePage.java
+++ src/main/java/org/example/pages/BasePage.java
@@ -11,7 +11,6 @@ import java.time.Duration;
 public class BasePage {
     public WebDriver driver;
     public WebDriverWait wait;
-
     public BasePage(WebDriver driver) {
         this.driver = driver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
